e2f76d4a4f166ccc2a6b861d2dfe417f6c0cc536
jrover88 <jeka19888@gmail.com>
2021-11-16 08:13:16 +0200

    First commit

---
+++ src/main/java/com/eugen/controller/MachineWindowController.java
@@ -0,0 +1,98 @@
+package com.eugen.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+
+public class MachineWindowController {
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private StackPane StackPane;
+
+    @FXML
+    private Label balance;
+
+    @FXML
+    void addAOnAction() {
+
+    }
+
+    @FXML
+    void addBOnAction() {
+
+    }
+
+    @FXML
+    void addCOnAction() {
+
+    }
+
+    @FXML
+    void addDOnAction() {
+
+    }
+
+    @FXML
+    void addEightOnAction() {
+
+    }
+
+    @FXML
+    void addFiveOnAction() {
+
+    }
+
+    @FXML
+    void addFourOnAction() {
+
+    }
+
+    @FXML
+    void addNineOnAction() {
+
+    }
+
+    @FXML
+    void addOneOnAction() {
+
+    }
+
+    @FXML
+    void addSevenOnAction() {
+
+    }
+
+    @FXML
+    void addThreeOnAction() {
+
+    }
+
+    @FXML
+    void addTwoOnAction() {
+
+    }
+
+    @FXML
+    void addZeroOnAction() {
+
+    }
+
+    @FXML
+    void insertMoneyButtonOnAction() {
+
+    }
+
+    @FXML
+    void okButtonOnAction() {
+
+    }
+
+    @FXML
+    void updateButtonOnAction() {
+
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module FxVendingMachine {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+
+    opens com.eugen;
+    opens com.eugen.view;
+    opens com.eugen.controller;
+}
